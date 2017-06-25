c28475425842af7aed73814c6638a3c8e47ba3b2
James Routley <jroutley@gmail.com>
2017-06-25 13:18:39 -0400

    Switch to tabs with 8 spaces

--- src/forth.asm
+++ src/forth.asm
@@ -1,26 +1,26 @@
 ; Magic ints
-sys_exit	equ		1
-sys_read	equ		3
-sys_write	equ		4
-stdin		equ		0
-stdout		equ		1
-stderr		equ		2
-inputlen	equ		128
+sys_exit	equ	1
+sys_read	equ	3
+sys_write	equ	4
+stdin		equ	0
+stdout		equ	1
+stderr		equ	2
+inputlen	equ	128
 
 section		.text
-global		main	
+global		main
 extern		printf
 
 main:
-	; mov		esi, instr
-	; jmp		next
-	jmp		accept
+	; mov	esi, instr
+	; jmp	next
+	jmp	accept
 
 ; Built in words:
 bye:
-	mov		ebx, 0
-	mov		eax, 1
-	int		0x80
+	mov	ebx, 0
+	mov	eax, 1
+	int	0x80
 
 dot:
 	; printf requires two parameters be pushed to the stack.
@@ -31,85 +31,85 @@ dot:
 	; - push value to print
 	push	dword fmt
 	call	printf
-	add		esp, 8
-	jmp		next
+	add	esp, 8
+	jmp	next
 
 dup:
-	pop		eax
+	pop	eax
 	push 	eax
 	push 	eax
-	jmp		next
+	jmp	next
 
 doliteral:
 	push	dword [esi]
-	add		esi, 4
-	jmp		next
+	add	esi, 4
+	jmp	next
 
 over:
 	; TODO: This could be done without popping - just read 'a' off the stack
 	; and push it.
-	pop		ebx
-	pop		eax
+	pop	ebx
+	pop	eax
 	push	eax
 	push	ebx
 	push	eax
-	jmp		next
+	jmp	next
 
 rot:
-	pop		ecx
-	pop		ebx
-	pop		eax
+	pop	ecx
+	pop	ebx
+	pop	eax
 	push	ebx
 	push	ecx
 	push	eax
-	jmp		next
+	jmp	next
 
 star:
-	pop		eax
-	pop		ebx
+	pop	eax
+	pop	ebx
 	imul	eax, ebx
 	push	eax
-	jmp		next
+	jmp	next
 
 squared:
 	call	enter
-	dd 		dup, star, exit
+	dd 	dup, star, exit
 
 swap:
-	pop		eax
-	pop		ebx
+	pop	eax
+	pop	ebx
 	push	eax
 	push	ebx
-	jmp		next
+	jmp	next
 
 ; Forth language
 
 accept:
-	mov		edx, inputlen 
-	mov		ecx, input
-	mov		ebx, stdin
-	mov		eax, sys_read 
-	int		0x80
+	mov	edx, inputlen
+	mov	ecx, input
+	mov	ebx, stdin
+	mov	eax, sys_read
+	int	0x80
 
 enter:
 	; Push esi to retstk
-	mov		eax, retstkptr
-	mov		[eax], esi
-	add		dword [retstkptr], 4
-	pop		eax
-	mov		esi, eax
-	jmp		next
+	mov	eax, retstkptr
+	mov	[eax], esi
+	add	dword [retstkptr], 4
+	pop	eax
+	mov	esi, eax
+	jmp	next
 
 exit:
 	; Pop esi from retstk
-	sub		dword [retstkptr], 4
-	mov		esi, [retstkptr]
-	jmp		next
+	sub	dword [retstkptr], 4
+	mov	esi, [retstkptr]
+	jmp	next
 
 next:
-	mov		eax, esi
-	add		esi, 4
-	jmp		[eax]
+	mov	eax, esi
+	add	esi, 4
+	jmp	[eax]
 
 section 	.data
 
@@ -117,4 +117,4 @@ retstk		times 16 dd 0
 retstkptr	dd retstk
 input		times inputlen db 0
 instr		dd doliteral, 5, doliteral, 7, over, dot, dot, dot, bye
-fmt			db	`%d\n`
+fmt		db `%d\n`
